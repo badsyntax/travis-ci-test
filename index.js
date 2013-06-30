bf51ffc1bf25feddf9988466489b206f97d9f465
Richard Willis <willis.rh@gmail.com>
2013-06-30 10:30:52 +0100

    Test

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var spawn = require('child_process').spawn;
-var child = spawn('sass', ['--scss']);
+var child = spawn('ls');
 
 child.stdout.on('data', function (data) {
-  console.log('Success');
+  console.log('Success: ' + data);
   process.exit(0);
 });
 
 child.stderr.on('data', function (data) {
-  console.log('Fail');
+  console.log('Fail: ' + data);
   process.exit(1);
 });
 
-child.stdin.write('.mystyle{color:red;}');
-child.stdin.end();
-
+// child.stdin.write('.mystyle{color:red;}');
+// child.stdin.end();
